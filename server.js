ba0b995258e4e18f1c7d416d39ca0219229f796e
Ashequl Haque <ashequlhaque009@gmail.com>
2021-10-27 11:54:43 -0400

    refactor migrations and add cors middleware

--- server.js
+++ server.js
@@ -1,52 +1,15 @@
 const express = require('express');
-const db = require('./database/seeder');
-const aggregate = require('./database/aggregation');
+const database = require('./database/dataMigration');
 
 const app = express();
-let isDbAlreadyPopulated = true; // change it later when deploying
 
-// The following methods should run only one time
-if (!isDbAlreadyPopulated) {
-  db.populateDatabase();
-  // make isDbAlreadyPopulated true
-}
-
-// aggregate.createProductInformationsCollection()
-//   // eslint-disable-next-line no-unused-vars
-//   .then((result) => {
-//     console.log('Created prodcutinformations collection successfully');
-//   })
-//   .catch((error) => {
-//     console.log('Error happened when creating prodcutinformations table ', error);
-//   });
-
-// aggregate.createStylesWithPhotosAndSkuCollection()
-//   // eslint-disable-next-line no-unused-vars
-//   .then((result) => {
-//     console.log('Created createStylesWithPhotosAndSkuCollection collection successfully');
-//   })
-//   .catch((error) => {
-//     console.log('Error happened when creating createStylesWithPhotosAndSkuCollection table ', error);
-//   });
-
-// aggregate.createProductStyleCollection()
-//   // eslint-disable-next-line no-unused-vars
-//   .then((result) => {
-//     console.log('Successfully created the productstyles collection');
-//   })
-//   .catch((err) => {
-//     console.log('Error happened while creating the productstyles collection ', err);
-//   });
-
-aggregate.createRelatedProductCollection()
-  // eslint-disable-next-line no-unused-vars
-  .then((results) => {
-    console.log('Successfully created relatedproducts collection');
+database.migrateData()
+  .then((done) => {
+    console.log('Data Migration Successful ', done);
   })
   .catch((error) => {
-    console.log('Error happened while creating the relatedproducts collection ', error);
+    console.log('Database migration failed ', error);
   });
-
 const port = 3333;
 app.listen(port, () => {
   console.log('Atelier Server is running on PORT ', port);
