772201417618bc5a432beac0df417d4894884921
Ashequl Haque <ashequlhaque009@gmail.com>
2021-10-26 23:59:39 -0400

    completed the aggregation pipeline

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const db = require('./database/seeder');
+const aggregate = require('./database/aggregation');
 
 const app = express();
+let isDbAlreadyPopulated = true; // change it later when deploying
 
-// db.populateDatabase();
+// The following methods should run only one time
+if (!isDbAlreadyPopulated) {
+  db.populateDatabase();
+  // make isDbAlreadyPopulated true
+}
+
+// aggregate.createProductInformationsCollection()
+//   // eslint-disable-next-line no-unused-vars
+//   .then((result) => {
+//     console.log('Created prodcutinformations collection successfully');
+//   })
+//   .catch((error) => {
+//     console.log('Error happened when creating prodcutinformations table ', error);
+//   });
+
+// aggregate.createStylesWithPhotosAndSkuCollection()
+//   // eslint-disable-next-line no-unused-vars
+//   .then((result) => {
+//     console.log('Created createStylesWithPhotosAndSkuCollection collection successfully');
+//   })
+//   .catch((error) => {
+//     console.log('Error happened when creating createStylesWithPhotosAndSkuCollection table ', error);
+//   });
+
+// aggregate.createProductStyleCollection()
+//   // eslint-disable-next-line no-unused-vars
+//   .then((result) => {
+//     console.log('Successfully created the productstyles collection');
+//   })
+//   .catch((err) => {
+//     console.log('Error happened while creating the productstyles collection ', err);
+//   });
+
+aggregate.createRelatedProductCollection()
+  // eslint-disable-next-line no-unused-vars
+  .then((results) => {
+    console.log('Successfully created relatedproducts collection');
+  })
+  .catch((error) => {
+    console.log('Error happened while creating the relatedproducts collection ', error);
+  });
 
 const port = 3333;
 app.listen(port, () => {
